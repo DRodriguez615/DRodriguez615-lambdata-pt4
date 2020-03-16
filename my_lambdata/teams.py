e4c0430f5c601865e4e9e8bef2f0eff7e995c041
DRodriguez615 <Drjrodriguez615@gmail.com>
2020-03-16 17:13:34 -0400

    Default Params

--- my_lambdata/teams.py
+++ my_lambdata/teams.py
@@ -1,43 +1,57 @@
 # example teams.py (functional approach)
 
 class Team():
-    def __init__(self, name, city):
+    def __init__(self, name, city, players=["Player 1"]):
         self.name = name
         self.city = city
+        self.players = players
 
-def advertise(my_team):
-    print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
+    def advertise(self):
+        # print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
+        print(f"HEY COME TO {self.city.upper()} TO SEE OUR GAMES!!!")
+        
 
-def full_name(my_team):
-    return f"{my_team['city']} {my_team['name']}"
+    @property
+    def full_name(self):
+        # return f"{my_team['city']} {my_team['name']}"
+        return f"{self.city} {self.name}"
 
 if __name__ == "__main__":
-    # teams = [
-    #     {"city": "New York", "name": "Yankees"},
-    #     {"city": "New York", "name": "Mets"},
-    #     {"city": "Boston", "name": "Red Sox"},
-    #     {"city": "New Haven", "name": "Ravens"},
-    #     {"city": "Washington", "name": "Nationals"}
-    # ]
-# 
-    # for team in teams:
-    #     print("-------")
-    #     print(full_name(team))
-    #     advertise(team)
+    teams = [
+        {"city": "New York", "name": "Yankees"},
+        {"city": "New York", "name": "Mets"},
+        {"city": "Boston", "name": "Red Sox"},
+        {"city": "New Haven", "name": "Ravens"},
+        {"city": "Washington", "name": "Nationals"}
+    ]
+ 
+    for team_attributes in teams:
+        team = Team(name = team_attributes["name"], city = team_attributes["city"])
+        print("-------")
+        # print(full_name(team))
+        # advertise(team)
+        print(team.city)
+        print(team.full_name)
+        print(team.players)
+        team.advertise()
+        
 
+
+
+    
     # df1 = DataFrame({"x": [1,2,3,], "y":[4,5,6,]})
     # df1.head()
     # df2 = DataFrame({"x": [7,7,7], "y": [4,4,4]})
     # df2.head()
      
-    team = Team(city= "Washington", name="Wizards") # initialize / create an instance
-    print(team)
-    print(type(team))
-    print(team.name) # invoking attributes
-    print(team.city) # invoking attributes
+    # team = Team(city= "Washington", name="Wizards") # initialize / create an instance
+    # print(team)
+    # print(type(team))
+    # print(team.name) # invoking attributes
+    # print(team.city) # invoking attributes
 
-    team2 = Team(city= "Giants", name="New York") # initialize / create an instance
-    print(team2)
-    print(type(team2))
-    print(team2.name) # invoking attributes
-    print(team2.city) # invoking attributes
+    # team2 = Team(city= "Giants", name="New York") # initialize / create an instance
+    # print(team2)
+    # print(type(team2))
+    # print(team2.name) # invoking attributes
+    # print(team2.city) # invoking attributes
